87054d78d2d6ea7698c6b198f812e386ab4f4b55
Batuhan ŞAHİN <batuhan.sahin@plusclouds.com>
2021-06-23 23:39:39 +0300

    1000 data seed added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\CompanyPackage::factory(10)->create();
+         \App\Models\CompanyPackage::factory(1000)->create();
     }
 }
